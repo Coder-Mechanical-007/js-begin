ddd209def40eb787899a28f5c970f66d0f621911
Mechanical Coder <123720930+Coder-Mechanical-007@users.noreply.github.com>
2024-06-19 10:28:38 +0000

    dataTypes

---
+++ js_1/dataType.js
@@ -0,0 +1,37 @@
+"use strict"
+
+// alert("Hello World")
+
+// console.log(7 + 9)
+
+
+let num1 = "99"
+let num2 = "777abc"
+console.log(typeof num1)
+
+let changenum = Number(num1);
+let changenum2 = Number(num2);
+console.log(typeof changenum)
+console.log(typeof changenum2)
+console.log(changenum2)
+
+let x = null;
+let y = undefined;
+console.log(typeof x)
+console.log(typeof y)
+let xy = Number(x);
+let yx = Number(y);
+console.log(xy);
+console.log(yx);
+
+// true => 1 Conversion
+// false => 0 Conversion
+
+
+// 1 => true; 0 => false
+// "" => false;
+// "name" => true
+
+
+
+
